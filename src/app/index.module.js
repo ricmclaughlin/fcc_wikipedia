4d0310ce681a1100dab59a7c7a14ddf4f75efff5
ric mclaughlin <ric@mclaughlin.today>
2016-05-09 09:42:25 -0700

    Update 2016-05-09T16:42:24.033Z

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('fccWikipedia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
